83e993ded1a82656e00068008f3cf7b02cf3b220
Myke Bates <contact@mykebates.com>
2016-12-09 22:45:08 -0600

    angular cloud ios push

--- Application Code/HelpSGF/src/pages/home/home.ts
+++ Application Code/HelpSGF/src/pages/home/home.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+import {Push, PushToken} from '@ionic/cloud-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public push: Push) {
+        this.push.register().then((t: PushToken) => {
+            return this.push.saveToken(t);
+        }).then((t: PushToken) => {
+            console.log('Token saved:', t.token);
+        });
 
-  }
+        this.push.rx.notification()
+            .subscribe((msg) => {
+                alert(msg.title + ': ' + msg.text);
+            });
+    }
 
 }
