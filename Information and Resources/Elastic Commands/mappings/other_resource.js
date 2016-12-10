2f035f35c1d5ca8864ed756b2d19b617d2198079
Tom Rankin <tom@tomrankin.net>
2016-12-09 23:28:34 -0600

    data

--- Information and Resources/Elastic Commands/mappings/other_resource.js
+++ Information and Resources/Elastic Commands/mappings/other_resource.js
@@ -1,8 +1,7 @@
 {
     "properties": {
-	    "id":					{ "type":"string", 	"index":"not_analyzed"},
 		"name":				{ "type": "string", "analyzer":"custom_analyzer" },
 		"resource_type":  	{ "type": "string" },
-        "geo_point":		{ "type": "geo_point" }
+        "location":			{ "type": "geo_point" }
     }
 }
