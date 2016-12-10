b4389f85f676ab9737b02481a33df86b5bdfcecf
Myke Bates <contact@mykebates.com>
2016-12-09 23:59:57 -0600

    catchign plugin

--- Application Code/HelpSGF/src/pages/about/about.ts
+++ Application Code/HelpSGF/src/pages/about/about.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
+
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+        Geolocation.getCurrentPosition().then((resp) => {
+            alert(resp.coords.latitude);
+        }).catch((error) => {
+            console.log('Error getting location', error);
+        });
+
+    }
 
 }
--- Application Code/HelpSGF/src/pages/home/home.ts
+++ Application Code/HelpSGF/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 
 import {NavController} from 'ionic-angular';
 import {Push, PushToken} from '@ionic/cloud-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
@@ -9,17 +10,20 @@ import {Push, PushToken} from '@ionic/cloud-angular';
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController, public push: Push) {
-        this.push.register().then((t: PushToken) => {
-            return this.push.saveToken(t);
-        }).then((t: PushToken) => {
-            console.log('Token saved:', t.token);
-        });
+    constructor(public navCtrl: NavController, public push: Push, private platform: Platform) {
 
-        this.push.rx.notification()
-            .subscribe((msg) => {
-                alert(msg.title + ': ' + msg.text);
+        if (platform.is('cordova')) {
+            this.push.register().then((t: PushToken) => {
+                return this.push.saveToken(t);
+            }).then((t: PushToken) => {
+                console.log('Token saved:', t.token);
             });
+
+            this.push.rx.notification()
+                .subscribe((msg) => {
+                    alert(msg.title + ': ' + msg.text);
+             });
+        }
     }
 
 }
