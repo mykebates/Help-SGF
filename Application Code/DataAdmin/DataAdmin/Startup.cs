88538c68a6be43f01803919d3e8eed54a7dc6f62
Tom Rankin <tom@tomrankin.net>
2016-12-10 02:25:09 -0600

    adding data admin

---
+++ Application Code/DataAdmin/DataAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataAdmin.Startup))]
+namespace DataAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
