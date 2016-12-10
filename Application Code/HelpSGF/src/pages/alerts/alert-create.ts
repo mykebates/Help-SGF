35694db91fb6be94580b7fcfa3cbad2c5f325234
Tom Rankin <tom@tomrankin.net>
2016-12-10 09:32:04 -0600

    alert me

--- Application Code/HelpSGF/src/pages/alerts/alert-create.ts
+++ Application Code/HelpSGF/src/pages/alerts/alert-create.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, ViewController} from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 @Component({
     selector: 'page-alert-create',
@@ -7,11 +8,28 @@ import {NavController, ViewController} from 'ionic-angular';
 })
 
 export class AlertCreatePage {
-
+    public base64Image: string;
     constructor(public navCtrl: NavController, public viewCtrl: ViewController) {
     }
 
     dismiss() {
         this.viewCtrl.dismiss();
+    }
+
+    getPicture(){
+        Camera.getPicture({
+            destinationType: Camera.DestinationType.DATA_URL,
+            targetWidth: 600,
+            targetHeight: 600,
+            allowEdit : false,
+            saveToPhotoAlbum : false,
+            correctOrientation: true
+        }).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            this.base64Image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+        });
     }
 }
--- Application Code/HelpSGF/src/pages/home/home.ts
+++ Application Code/HelpSGF/src/pages/home/home.ts
@@ -2,14 +2,14 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Push, PushToken} from '@ionic/cloud-angular';
 import { Platform } from 'ionic-angular';
-import { Camera } from 'ionic-native';
+
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
 export class HomePage {
-    public base64Image: string;
+
     constructor(public navCtrl: NavController, public push: Push, private platform: Platform) {
 
         if (platform.is('cordova')) {
@@ -27,21 +27,6 @@ export class HomePage {
     }
 
 
-    cameraTest(){
-        Camera.getPicture({
-            destinationType: Camera.DestinationType.DATA_URL,
-            targetWidth: 600,
-            targetHeight: 600,
-            allowEdit : false,
-            saveToPhotoAlbum : false,
-            correctOrientation: true
-        }).then((imageData) => {
-            // imageData is either a base64 encoded string or a file URI
-            // If it's base64:
-            this.base64Image = 'data:image/jpeg;base64,' + imageData;
-        }, (err) => {
-            // Handle error
-        });
-    }
+
 
 }
